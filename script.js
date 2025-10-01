bab003c6111f45b76c543e5b21a48684edc81358
Mohammad Wali <wali.afghan22@gmail.com>
2025-10-01 16:50:41 +0500

    update
    update

---
+++ script.js
@@ -0,0 +1,12 @@
+// Smooth scrolling for navigation links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// Mobile menu toggle
+const hamburger = document.querySelector
